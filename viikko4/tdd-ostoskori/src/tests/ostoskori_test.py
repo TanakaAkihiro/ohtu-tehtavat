778d8b39e90d2ea3c27dd288d2e8408a657907bc
Akihiro Tanaka <akihiro.fin@gmail.com>
2021-11-27 12:16:13 +0200

    vk4 t5.1, lisätty testi

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -8,3 +8,15 @@ class TestOstoskori(unittest.TestCase):
 
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
         self.assertEqual(self.kori.hinta(), 0)
+    
+    def test_palauttaa_oikean_maaran_tavaroita(self):
+        self.assertEqual(self.kori.tavaroita_korissa, 0)
+
+        maito = Tuote("maito", 5)
+        suklaa = Tuote("maito", 7)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(suklaa)
+
+        self.assertEqual(self.kori.tavaroita_korissa, 3)
+ 
