208aa5b5121d222592053f469133439d6d9b74ab
Akihiro Tanaka <akihiro.fin@gmail.com>
2021-11-28 14:01:26 +0200

    vk4 t5.13 testi menee läpi

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -45,7 +45,9 @@ class Ostoskori:
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
-        pass
+        for ostos in self.ostoslista:
+            if ostos.tuotteen_nimi() == poistettava.nimi() and ostos.hinta()/ostos.lukumaara() == poistettava.hinta():
+                ostos.muuta_lukumaaraa(-1)
 
     def tyhjenna(self):
         pass
