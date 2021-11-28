e1623dd3a9ff7f2b855f59b9446a6c8b10935dbb
Akihiro Tanaka <akihiro.fin@gmail.com>
2021-11-28 15:11:43 +0200

    vk4 t6 poistettu metodi kopioi taulukko

--- viikko4/int-joukko/src/int_joukko.py
+++ viikko4/int-joukko/src/int_joukko.py
@@ -1,3 +1,5 @@
+from copy import copy
+
 class IntJoukko:
     def __init__(self, kapasiteetti=5, kasvatuskoko=5):
         if not isinstance(kapasiteetti, int) or kapasiteetti < 0:
@@ -21,10 +23,7 @@ class IntJoukko:
             self.alkioiden_lkm = self.alkioiden_lkm + 1
 
             if self.alkioiden_lkm % len(self.lukujono) == 0:
-                taulukko_old = self.lukujono
-                self.kopioi_taulukko(self.lukujono, taulukko_old)
-                self.lukujono = [0] * (self.alkioiden_lkm + self.kasvatuskoko)
-                self.kopioi_taulukko(taulukko_old, self.lukujono)
+                self.lukujono += [0] * self.kasvatuskoko
 
             return True
 
@@ -50,10 +49,6 @@ class IntJoukko:
             return True
 
         return False
-
-    def kopioi_taulukko(self, a, b):
-        for i in range(0, len(a)):
-            b[i] = a[i]
 
     def mahtavuus(self):
         return self.alkioiden_lkm
