7370502d470e18cc03bae44851ba32c8e69379b0
Akihiro Tanaka <akihiro.fin@gmail.com>
2021-12-03 14:16:45 +0200

    vk5 t3, luo apulista tasatilannetta varten

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -4,6 +4,12 @@ class TennisGame:
         self.player2_name = player2_name
         self.m_score1 = 0
         self.m_score2 = 0
+        self.tie_game = [
+            "Love-All",
+            "Fifteen-All",
+            "Thirty-All",
+            
+        ]
 
     def won_point(self, player_name):
         if player_name == "player1":
@@ -16,16 +22,10 @@ class TennisGame:
         temp_score = 0
 
         if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
-                score = "Love-All"
-            elif self.m_score1 == 1:
-                score = "Fifteen-All"
-            elif self.m_score1 == 2:
-                score = "Thirty-All"
-            elif self.m_score1 == 3:
-                score = "Forty-All"
-            else:
+            if self.m_score1 > 3:
                 score = "Deuce"
+            else:
+                score = self.tie_game[self.m_score1]
         elif self.m_score1 >= 4 or self.m_score2 >= 4:
             minus_result = self.m_score1 - self. m_score2
 
